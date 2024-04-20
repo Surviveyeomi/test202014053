f253f9550a6ae7add164f44a207ee9937a503198
Surviveyeomi <yeomi189@gmail.com>
2024-04-20 17:44:54 +0900

    two

--- src/test202014053/Main.java
+++ src/test202014053/Main.java
@@ -3,6 +3,7 @@ package test202014053;
 public class Main {
 	
 	public static void main(String[] args) {
+		Two.print();
 		System.out.println("one 202014053");
 		
 	}
---
+++ src/test202014053/Two.java
@@ -0,0 +1,8 @@
+package test202014053;
+
+public class Two {
+
+  public static void print() {
+     System.out.println("two"); 
+    }
+}
